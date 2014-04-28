edbb1483665db1cfa2d6d40025e8ffaa14227637
Alex Torres <alexst07@gmail.com>
2014-04-27 22:26:44 -0300

    Upload examples lua file

---
+++ examples/waitfilter.lua
@@ -0,0 +1,23 @@
+--
+--  This file is part of the Naphex project
+--  Copyright (C) 2014  Alex Silva Torres <alexst07@gmail.com>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+wf = wait_filter.new("eth0");
+wf:dev_promisc(1);
+wf:set_file("../tests/waitfilter_f.lua");
+wf:set_filter("wfilter");
+wf:set_maxtime(2);
+wf:wait()
